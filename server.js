60317f886189b3c82e42fa6db3ab3084b150098c
RyanSpeirs <61259710+RyanSpeirs@users.noreply.github.com>
2025-03-21 19:38:58 +1100

    basic setup

--- server.js
+++ server.js
@@ -3,9 +3,20 @@
 
 var express = require('express')
 var app = express()
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
+var bodyParser = require('body-parser')
 
 app.use(express.static(__dirname))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
+
 var server = app.listen(3000, () => {
     console.log('server is listening on port', server.address().port)
+})
+
+io.on('connection', (socket) => {
+    console.log('a user connected')
 })
 
