7c6f97e85f410d8df0a095a217fb26ba6348bba1
RyanSpeirs <61259710+RyanSpeirs@users.noreply.github.com>
2025-03-22 14:43:45 +1100

    Final

--- server.js
+++ server.js
@@ -1,23 +1,25 @@
-// SIT 323 2.1P
-// Ryan Speirs 600152989
+//  SIT 323 2.1P
+//  Ryan Speirs 600152989
 
+//  calling all the modules we need, express and socket.io
+//  giving them the variables to make it easy to invoke them
 var express = require('express')
 var app = express()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
 var bodyParser = require('body-parser')
 
+//  Calling the html and css in the current directory, needs to be in same directory to work obviously
 app.use(express.static(__dirname))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
 
+//  I contemplated putting a webform in, but its not required
+//  app.use(bodyParser.json())
+//  app.use(bodyParser.urlencoded({extended: false}))
 
 // Tells us when someone connects, when it works, which isn't often
 io.on('connection', (socket) => { 
     console.log('a user connected')
 })
-
-
 
 // sets the server to listen to port 3000 and notifies us that it is doing so
 var server = app.listen(3000, () => {
